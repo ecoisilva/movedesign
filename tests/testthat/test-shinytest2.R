fc4af5c78655843d4b137c5684906df3b025482c
Inês <imss.silva@gmail.com>
2023-02-02 15:00:10 +0100

    :memo: expand tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,22 +1,49 @@
 
 test_that("{shinytest2} recording: app runs", {
+  # skip_on_cran() # potential issue with CRAN build servers
   
   shiny_app <- movedesign::run_app()
   app <- AppDriver$new(shiny_app, name = "workflow")
-
+  
   app$set_inputs(`tab_about_1-which_data` = "Select")
   app$set_inputs(`tab_about_1-which_question` = "Home range")
   
-  app$expect_values(input = "tab_about_1-which_question", screenshot_args = FALSE)
+  app$expect_values(input = "tab_about_1-which_question", 
+                    screenshot_args = FALSE)
   
   out <- app$get_values()
-  expect_identical(out$input$`tab_about_1-which_question`, "Home range")
+  expect_identical(
+    out$input$`tab_about_1-which_question`,
+    "Home range")
   
   app$stop()
 })
 
 test_that("{shinytest2} recording: gps simulation works", {
-  skip_on_os(os = "linux") # issue with wait_for_idle()
+  # skip_on_cran() # potential issue with CRAN build servers
+  
+  shiny_app <- movedesign::run_app()
+  app <- AppDriver$new(shiny_app, name = "gps")
+  
+  app$set_inputs(tabs = "regime")
+  app$set_inputs(`tab_device_1-device_type` = "GPS")
+  
+  app$wait_for_value(output = "tab_device_1-regPlot_gps",
+                     ignore = list(NULL))
+  
+  out <- app$get_values()
+  expect_identical(
+    !is.null(out$output$`tab_device_1-regPlot_gps`), 
+    TRUE)
+  
+  app$stop()
+})
+
+
+test_that("{shinytest2} recording: gps plot selection", {
+  # skip_on_cran() # potential issue with CRAN build servers
+  skip_on_os(os = c("mac", "linux")) 
+  # issues with wait_for_idle(), wait_for_value()
   
   shiny_app <- movedesign::run_app()
   app <- AppDriver$new(shiny_app, name = "gps")
@@ -24,7 +51,10 @@ test_that("{shinytest2} recording: gps simulation works", {
   
   app$set_inputs(tabs = "regime")
   app$set_inputs(`tab_device_1-device_type` = "GPS")
-  app$wait_for_idle(1000)
+  
+  # app$wait_for_idle(1000)
+  app$wait_for_value(output = "tab_device_1-regPlot_gps",
+                     ignore = list(NULL))
   
   app$set_inputs(`tab_device_1-gps_dur` = 2)
   app$set_inputs(`tab_device_1-gps_dur_units` = "years")
@@ -32,7 +62,10 @@ test_that("{shinytest2} recording: gps simulation works", {
   
   app$set_inputs(`tab_device_1-regPlot_gps_selected` = "8", 
                  allow_no_input_binding_ = TRUE)
-  app$wait_for_idle(1000)
+  
+  # app$wait_for_idle(1000)
+  app$wait_for_value(output = "tab_device_1-regPlot_gps",
+                     ignore = list(NULL))
   
   app$click("tab_device_1-validate_gps")
   
@@ -46,3 +79,4 @@ test_that("{shinytest2} recording: gps simulation works", {
   
   app$stop()
 })
+
