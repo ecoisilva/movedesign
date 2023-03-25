d1a824eec0b760fa6b0f2c832db00e80c69eab0f
Inês <imss.silva@gmail.com>
2023-03-25 11:31:52 +0100

    :memo: update documentation

--- R/movedesign-package.R
+++ R/movedesign-package.R
@@ -1,35 +1,15 @@
+#' movedesign: Study design of movement ecology studies
+#' 
+#' The `movedesign` package contains an R shiny application that can be used
+#' to assist researchers in designing movement ecology studies.
+#'
 #' @keywords internal
 "_PACKAGE"
 
-utils::globalVariables(
-  c(".data", "group",
-    
-    "x", "y",
-    "long", "lat",
-    "longitude", "latitude",
-    "time", "timestamp", "lag",
-    
-    "duration", "dur",
-    "interval", "dti", "dti_notes",
-    
-    "error", "error_lci", "error_uci",
-    "CI_low", "CI_high",
-
-    "buffalo",
-    "coati",
-    "pelican",
-    "jaguar",
-    "wolf",
-    "gazelle",
-    "turtle"
-    ))
-
-## usethis namespace: start
 #' @importFrom ctmm %#%
 #' @importFrom dplyr %>%
 #' @importFrom ggplot2 %+replace%
 #' @importFrom stats median
 #' @importFrom utils data
 #' @importFrom utils packageVersion
-## usethis namespace: end
 NULL
