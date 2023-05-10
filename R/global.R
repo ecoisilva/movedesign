edfb6ebf76e3ba9cd6e0452f7ec1fe30b29a2fd4
Inês <imss.silva@gmail.com>
2023-05-10 18:26:42 +0200

    :memo: add startup msg

--- R/global.R
+++ R/global.R
@@ -1,26 +1,26 @@
 
-# .onLoad <- function(libname, pkgname) {
-#   rlang::run_on_load()
-# }
-# 
-# rlang::on_load(rlang::local_use_cli(inline = TRUE))
-# 
-# .onAttach <- function(libname, pkgname) {
-#   version <- utils::packageDescription(pkgname, fields = "Version")
-# 
-#   website <- "https://github.com/ecoisilva/movedesign/issues"
-#   if (interactive())
-#     website <- "{.url https://github.com/ecoisilva/movedesign/issues}"
-#   
-#   rlang::inform(
-#     class = "packageStartupMessage",
-#     message = c(
-#       "!" = paste("Issues?", website),
-#       "i" = 'Citation: \tFor citation details, run: {.code citation("movedesign")}',
-#       "x" = "Silence? {.code suppressPackageStartupMessages(library(movedesign))}"
-#     )
-#   )
-# }
+.onLoad <- function(libname, pkgname) {
+  rlang::run_on_load()
+}
+
+rlang::on_load(rlang::local_use_cli(inline = TRUE))
+
+.onAttach <- function(libname, pkgname) {
+  version <- utils::packageDescription(pkgname, fields = "Version")
+
+  website <- "https://github.com/ecoisilva/movedesign/issues"
+  if (interactive())
+    website <- "{.url https://github.com/ecoisilva/movedesign/issues}"
+
+  rlang::inform(
+    class = "packageStartupMessage",
+    message = c(
+      "!" = paste("Issues?", website),
+      "x" = "Silence? {.code suppressPackageStartupMessages(library(movedesign))}",
+      "i" = 'For citation details, run: {.code citation("movedesign")}'
+    )
+  )
+}
 
 # Global variables:
 utils::globalVariables(
@@ -47,16 +47,3 @@ utils::globalVariables(
     "gazelle",
     "turtle")
 )
-
-# Application layout:
-div_column_main <- "col-xs-12 col-sm-12 col-md-12 col-lg-12"
-div_column_half <- "col-xs-6 col-sm-6 col-md-6 col-lg-6"
-
-div_column_left <- "col-xs-12 col-sm-4 col-md-4 col-lg-3"
-div_column_right <- "col-xs-12 col-sm-8 col-md-8 col-lg-9"
-
-# Message types:
-msg_main <- crayon::make_style("dimgray")
-msg_success <- crayon::make_style("#009da0")
-msg_danger <- crayon::make_style("#dd4b39")
-msg_warning <- crayon::make_style("#ffbf00")
