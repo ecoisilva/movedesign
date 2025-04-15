8a0c72a85c2c39ed34e1160bd1cee066e9436325
ecoisilva <imss.silva@gmail.com>
2025-04-15 15:57:31 +0200

    :pencil: remove trailing whitespaces

--- app.R
+++ app.R
@@ -2,8 +2,8 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE, 
-                  helpers = FALSE, 
+pkgload::load_all(export_all = FALSE,
+                  helpers = FALSE,
                   attach_testthat = FALSE)
 
 options("golem.app.prod" = TRUE)
